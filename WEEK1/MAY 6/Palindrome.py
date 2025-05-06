34d95c9af6b766fd5fee4bba078b9c4d98f33952
Alinageorge <alinagrg03@gmail.com>
2025-05-06 11:25:46 +0530

    updated with proper commenting

--- WEEK1/MAY 6/Palindrome.py
+++ WEEK1/MAY 6/Palindrome.py
@@ -1,8 +1,9 @@
 def Palindrome(word):
-    term=word.lower()
+    term=word.lower()       #converting string to lower case inorder to compare words irrespective of type
     print("Word: ",term)
-    rev=term[::-1]
+    rev=term[::-1]          #reversing the word
     print("Reverse: ",rev)
+    #comparing word and its reverse
     if term==rev:
         return True
     else:
