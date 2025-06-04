b6ca776869ec6a13520bf9b2ad2756bc7aa3de38
Alinageorge <alinagrg03@gmail.com>
2025-06-04 12:18:24 +0530

    Implementing user authentication in DRF

--- WEEK 5/JUNE 2/myproject/settings.py
+++ WEEK 5/JUNE 2/myproject/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'tasks', 
     'rest_framework',
-    'tasks',  
+    'rest_framework.authtoken' 
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#to add authentication and permission classes
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
--- WEEK 5/JUNE 2/tasks/views.py
+++ WEEK 5/JUNE 2/tasks/views.py
@@ -1,7 +1,13 @@
 from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated      #to enforce authentication
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]      #to enforce authentication
+
+
+
+    
